4f9d739450c9548db5d00af0059c3628380d2bc1
Henio Junior <henioajunior@gmail.com>
2022-08-22 13:40:34 -0700

    Criação do TopicoService

--- forum/src/main/kotlin/br/com/crystaldata/forum/controller/TopicoController.kt
+++ forum/src/main/kotlin/br/com/crystaldata/forum/controller/TopicoController.kt
@@ -3,6 +3,7 @@ package br.com.crystaldata.forum.controller
 import br.com.crystaldata.forum.model.Curso
 import br.com.crystaldata.forum.model.Topico
 import br.com.crystaldata.forum.model.Usuario
+import br.com.crystaldata.forum.service.TopicoService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -10,28 +11,11 @@ import java.util.Arrays
 
 @RestController
 @RequestMapping("/topicos")
-class TopicoController {
+class TopicoController(private val service: TopicoService) {
 
     @GetMapping
     fun listar(): List<Topico> {
-        val topico = Topico(
-            id = 1,
-            titulo = "Duvida Kotlin",
-            mensagem = "Variaveis do Kotlin",
-            curso = Curso(
-                id = 1,
-                nome = "Kotlin",
-                categoria = "Programação"
-            ),
-            autor = Usuario(
-                id = 1,
-                nome = "Ana da Silva",
-                email = "ana@email.com"
-            )
-
-        )
-
-        return Arrays.asList(topico, topico, topico)
+        return service.listar()
     }
 
 }
---
+++ forum/src/main/kotlin/br/com/crystaldata/forum/service/TopicoService.kt
@@ -0,0 +1,69 @@
+package br.com.crystaldata.forum.service
+
+import br.com.crystaldata.forum.model.Curso
+import br.com.crystaldata.forum.model.Topico
+import br.com.crystaldata.forum.model.Usuario
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class TopicoService(private var topicos: List<Topico>) {
+
+    init {
+        val topico1 = Topico(
+            id = 1,
+            titulo = "Duvida Kotlin1",
+            mensagem = "Variaveis do Kotlin1",
+            curso = Curso(
+                id = 1,
+                nome = "Kotlin",
+                categoria = "Programação"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Ana da Silva",
+                email = "ana@email.com"
+            )
+        )
+
+        val topico2 = Topico(
+            id = 2,
+            titulo = "Duvida Kotlin2",
+            mensagem = "Variaveis do Kotlin2",
+            curso = Curso(
+                id = 1,
+                nome = "Kotlin",
+                categoria = "Programação"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Ana da Silva",
+                email = "ana@email.com"
+            )
+        )
+
+        val topico3 = Topico(
+            id = 3,
+            titulo = "Duvida Kotlin3",
+            mensagem = "Variaveis do Kotlin3",
+            curso = Curso(
+                id = 1,
+                nome = "Kotlin",
+                categoria = "Programação"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Ana da Silva",
+                email = "ana@email.com"
+            )
+        )
+        topicos = Arrays.asList(topico1, topico2, topico3)
+    }
+
+
+    fun listar(): List<Topico> {
+        return topicos
+    }
+
+
+}
