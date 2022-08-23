ae34f51703ac04fe66074f9ceda4255ebe09ee86
Henio Junior <henioajunior@gmail.com>
2022-08-23 10:18:07 -0700

    Atualizando um tópico

--- forum/src/main/kotlin/br/com/crystaldata/forum/controller/TopicoController.kt
+++ forum/src/main/kotlin/br/com/crystaldata/forum/controller/TopicoController.kt
@@ -1,11 +1,13 @@
 package br.com.crystaldata.forum.controller
 
+import br.com.crystaldata.forum.dto.AtualizacaoTopicoForm
 import br.com.crystaldata.forum.dto.NovoTopicoForm
 import br.com.crystaldata.forum.dto.TopicoView
 import br.com.crystaldata.forum.service.TopicoService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -26,7 +28,12 @@ class TopicoController(private val service: TopicoService) {
     }
 
     @PostMapping
-    fun cadastrar(@RequestBody @Valid dto: NovoTopicoForm) {
-        service.cadastrar(dto)
+    fun cadastrar(@RequestBody @Valid form: NovoTopicoForm) {
+        service.cadastrar(form)
+    }
+
+    @PutMapping
+    fun atualizar(@RequestBody @Valid form: AtualizacaoTopicoForm) {
+        service.atualizar(form)
     }
 }
--- forum/src/main/kotlin/br/com/crystaldata/forum/service/TopicoService.kt
+++ forum/src/main/kotlin/br/com/crystaldata/forum/service/TopicoService.kt
@@ -1,5 +1,6 @@
 package br.com.crystaldata.forum.service
 
+import br.com.crystaldata.forum.dto.AtualizacaoTopicoForm
 import br.com.crystaldata.forum.dto.NovoTopicoForm
 import br.com.crystaldata.forum.dto.TopicoView
 import br.com.crystaldata.forum.mapper.TopicoFormMapper
@@ -16,12 +17,12 @@ class TopicoService(
 ) {
 
     fun listar(): List<TopicoView> {
-        return topicos.stream().map{ t -> topicoViewMapper.map(t)}
+        return topicos.stream().map { t -> topicoViewMapper.map(t) }
             .collect(Collectors.toList())
     }
 
     fun buscarPorId(id: Long): TopicoView {
-        val topico =  topicos.stream().filter {t -> t.id == id}
+        val topico = topicos.stream().filter { t -> t.id == id }
             .findFirst().get()
         return topicoViewMapper.map(topico)
     }
@@ -30,5 +31,22 @@ class TopicoService(
         var topico = topicoFormMapper.map(form)
         topico.id = topicos.size.toLong() + 1
         topicos = topicos.plus(topico)
+    }
+
+    fun atualizar(form: AtualizacaoTopicoForm) {
+        val topico = topicos.stream().filter { t -> t.id == form.id }
+            .findFirst().get()
+        topicos = topicos.minus(topico).plus(
+            Topico(
+                id = form.id,
+                titulo = form.titulo,
+                mensagem = form.mensagem,
+                autor = topico.autor,
+                curso = topico.curso,
+                respostas = topico.respostas,
+                status = topico.status,
+                dataCriacao = topico.dataCriacao
+            )
+        )
     }
 }
