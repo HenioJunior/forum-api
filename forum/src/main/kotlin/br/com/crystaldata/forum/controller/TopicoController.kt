bfd0d109e181883c89d00ffb0cdc8313758ef308
Henio Junior <henioajunior@gmail.com>
2022-08-24 20:19:06 -0300

    Adicionando a anotacao @PageableDefault

--- forum/src/main/kotlin/br/com/crystaldata/forum/controller/TopicoController.kt
+++ forum/src/main/kotlin/br/com/crystaldata/forum/controller/TopicoController.kt
@@ -6,6 +6,7 @@ import br.com.crystaldata.forum.dto.TopicoView
 import br.com.crystaldata.forum.service.TopicoService
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -20,7 +21,7 @@ class TopicoController(private val service: TopicoService) {
     @GetMapping
     fun listar(
         @RequestParam(required = false) nomeCurso: String?,
-        paginacao: Pageable
+        @PageableDefault(size = 3) paginacao: Pageable
     ): Page<TopicoView> {
         return service.listar(nomeCurso, paginacao)
     }
