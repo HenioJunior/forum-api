0fe8b7ef9e63bde32bfe275d1ded5f4b938e1b8e
Henio Junior <henioajunior@gmail.com>
2022-09-23 16:28:31 -0300

    Instalado o SpringSecurity e Criado a Classe SecurityConfiguration

---
+++ README.md
@@ -0,0 +1,37 @@
+## Notas
+
+Criação da classe SecurityConfiguration
+----------------------------------------
+
+1. - Utilizar as anotações de classe: `@EnableWebSecurity` e `@Configuration`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
---
+++ forum/src/main/kotlin/br/com/crystaldata/forum/config/SecurityConfiguration.kt
@@ -0,0 +1,9 @@
+package br.com.crystaldata.forum.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+
+@EnableWebSecurity
+@Configuration
+class SecurityConfiguration {
+}
