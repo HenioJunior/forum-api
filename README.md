0e9bf3d3b6aeedaab1c92d1b848e4082c300934a
Henio Junior <henioajunior@gmail.com>
2022-09-23 16:58:35 -0300

    Implementado o método securityFilterChain

--- README.md
+++ README.md
@@ -3,7 +3,25 @@
 Criação da classe SecurityConfiguration
 ----------------------------------------
 
-1. - Utilizar as anotações de classe: `@EnableWebSecurity` e `@Configuration`
+   1. Utilizar as anotações de classe: `@EnableWebSecurity` e `@Configuration`
+   
+   2. Criação do método de autorização(Bloquear ou Liberar recursos):
+    ```java
+     @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http.
+        authorizeRequests().
+        anyRequest().
+        authenticated()
+            .and().
+            sessionManagement().
+            sessionCreationPolicy(SessionCreationPolicy.STATELESS)//Não vou guardar estado
+            .and().
+            formLogin().disable().httpBasic()
+        return http.build()
+    }
+    ```
+    
 
 
 
--- forum/src/main/kotlin/br/com/crystaldata/forum/config/SecurityConfiguration.kt
+++ forum/src/main/kotlin/br/com/crystaldata/forum/config/SecurityConfiguration.kt
@@ -1,9 +1,26 @@
 package br.com.crystaldata.forum.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
 
 @EnableWebSecurity
 @Configuration
 class SecurityConfiguration {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeRequests { authz -> authz
+                    .anyRequest().authenticated()
+            }
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .formLogin().disable().httpBasic()
+        return http.build()
+    }
 }
