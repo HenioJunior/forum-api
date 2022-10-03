a59a8ca14b321097b4555b533fcba1239099e9b2
Henio Junior <henioajunior@gmail.com>
2022-10-03 11:09:33 -0300

    Refactoring no método listar tópicos

--- forum/src/test/kotlin/br/com/crystaldata/forum/service/TopicoServiceTest.kt
+++ forum/src/test/kotlin/br/com/crystaldata/forum/service/TopicoServiceTest.kt
@@ -2,12 +2,19 @@ package br.com.crystaldata.forum.service
 
 import br.com.crystaldata.forum.mapper.TopicoFormMapper
 import br.com.crystaldata.forum.mapper.TopicoViewMapper
+import br.com.crystaldata.forum.model.TopicoTest
 import br.com.crystaldata.forum.repository.TopicoRepository
+import io.mockk.every
 import io.mockk.mockk
+import org.springframework.data.domain.PageImpl
 
 class TopicoServiceTest {
 
-    val topicoRepository: TopicoRepository = mockk()
+    val topicos = PageImpl(listOf(TopicoTest.build()))
+
+    val topicoRepository: TopicoRepository = mockk {
+        every { findByCursoNome(any(), any()) } returns topicos
+    }
     val topicoViewMapper: TopicoViewMapper = mockk()
     val topicoFormMapper: TopicoFormMapper = mockk()
 
